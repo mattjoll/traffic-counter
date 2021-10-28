34fea081fe411ebc4d14682a9d0fead8e163dfd6
Matthew Joll <dev@mattjoll.nz>
2021-10-28 17:31:47 +1300

    feat(counter): Add simple hook to increment and decrement the counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,28 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+
+enum ClickAction {
+  Decrement = 1,
+  Increment = 2,
+}
 
 const Counter: FunctionComponent = () => {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = (action: ClickAction) => {
+    if (action === ClickAction.Increment) {
+      setCount(count + 1);
+    } else if (count !== 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <main>
+      <p>{count}</p>
+      <button onClick={() => handleClick(ClickAction.Decrement)}>Remove</button>
+      <button onClick={() => handleClick(ClickAction.Increment)}>Add</button>
+    </main>
+  );
 };
 
 export default Counter;
