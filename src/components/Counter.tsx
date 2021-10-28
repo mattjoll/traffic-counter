abda062df7d993ca7b61c81939e56a321ee0b931
Matthew Joll <dev@mattjoll.nz>
2021-10-28 19:13:45 +1300

    feat(counter): Store counter data in session storage

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,9 +2,20 @@ import React, { FunctionComponent, useState } from 'react';
 import Button from './Button';
 
 enum ClickAction {
-  Decrement = 1,
-  Increment = 2,
+  Decrement = 'decrement',
+  Increment = 'increment',
 }
+
+const createDataPoint = (action: ClickAction) => {
+  const currentDbString = sessionStorage.getItem('db') || '{}';
+  const currentDb = JSON.parse(currentDbString);
+  const dbUpdate = {
+    ...currentDb,
+    [Date.now().toString()]: action,
+  };
+
+  sessionStorage.setItem('db', JSON.stringify(dbUpdate));
+};
 
 const Counter: FunctionComponent = () => {
   const [count, setCount] = useState(0);
@@ -12,15 +23,17 @@ const Counter: FunctionComponent = () => {
   const handleClick = (action: ClickAction) => {
     if (action === ClickAction.Increment) {
       setCount(count + 1);
+      createDataPoint(action);
     } else if (count !== 0) {
       setCount(count - 1);
+      createDataPoint(action);
     }
   };
 
   return (
     <section className="flex flex-col items-center">
       <div className="font-medium m-8 text-9xl">{count}</div>
-      <div className="space-x-4">
+      <div className="space-x-8">
         <Button onClick={() => handleClick(ClickAction.Decrement)}>
           Remove
         </Button>
